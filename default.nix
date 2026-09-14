93cb3e46a96052813ecd865379ae413322aca5d3
Amanogawa Subaru <amanogawa.subaru0705@gmail.com>
2026-09-14 17:18:00 +0800

    Wire Bedrock module configuration

--- default.nix
+++ default.nix
@@ -1,4 +1,18 @@
+{ lib, ... }:
+
+let
+  settings = import ./settings.nix;
+  username = settings.username;
+in
 {
+  _module.args = {
+    inherit username settings;
+  };
+
   imports = [
-  ];
+    ./modules/mount.nix
+    ./modules/user-packages.nix
+  ]
+  ++ lib.optional settings.nvidia ./modules/nvidia.nix
+  ++ lib.optional settings.nvidiaPrime ./modules/nvidia-prime.nix;
 }
