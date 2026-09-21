9bb9124009483a1fc7359aff12486e34fab125e7
Amanogawa Subaru <amanogawa.subaru0705@gmail.com>
2026-09-22 04:32:14 +0800

    added blacknote to profile catalog

--- profiles.nix
+++ profiles.nix
@@ -4,4 +4,10 @@
     repo = "https://github.com/amanogawa-subaru/whitebox.git";
     description = "whitebox desktop profile";
   };
+
+  blacknote = {
+    name = "blacknote";
+    repo = "https://github.com/amanogawa-subaru/blacknote.git";
+    description = "blacknote desktop profile";
+  };
 }
